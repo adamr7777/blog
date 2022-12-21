96b775818fd68694d1f7d489ca4ce296049f141a
adomas <adomas.razmus@gmail.com>
2022-12-21 21:37:07 +0000

    fetch.post

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ console.log(array2);
 //Endpoint: /post
 
 const container = document.getElementById('container');
+const formEl = document.getElementById('form-el');
+const titleInput = document.getElementById('title-input');
+const bodyInput = document.getElementById('body-input');
 
 
 fetch('https://apis.scrimba.com/jsonplaceholder/posts')
@@ -25,7 +28,19 @@ function renderPosts(blogPosts) {
     container.innerHTML += `<h2>${element.title}</h2>
                           <p>${element.body}</p>
                           <hr></hr>`
-
-
   }
 }
+
+
+
+formEl.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const post1 = {title: titleInput.value, body: bodyInput.value}
+  fetch('https://apis.scrimba.com/jsonplaceholder/posts', {
+    method: 'post',
+    body: JSON.stringify(post1),
+    headers: {'Content-Type': 'application/json'}
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+})
